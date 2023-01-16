4e6b38b909368a508d1497a9eed21ace833df54d
akie123 <aalhadakate@gmail.com>
2023-01-16 13:36:37 +0530

    first

---
+++ src/components/headers/header3.js
@@ -0,0 +1,163 @@
+import * as React from 'react';
+import PropTypes from 'prop-types';
+import AppBar from '@mui/material/AppBar';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import Grid from '@mui/material/Grid';
+import HelpIcon from '@mui/icons-material/Help';
+import IconButton from '@mui/material/IconButton';
+import Link from '@mui/material/Link';
+import MenuIcon from '@mui/icons-material/Menu';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+import Tab from '@mui/material/Tab';
+import Tabs from '@mui/material/Tabs';
+import Toolbar from '@mui/material/Toolbar';
+import Tooltip from '@mui/material/Tooltip';
+import Typography from '@mui/material/Typography';
+import MenuItem from "@mui/material/MenuItem";
+import Divider from "@mui/material/Divider";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import PersonAdd from "@mui/icons-material/PersonAdd";
+import Settings from "@mui/icons-material/Settings";
+import Logout from "@mui/icons-material/Logout";
+import Menu from "@mui/material/Menu";
+
+const lightColor = 'rgba(255, 255, 255, 0.7)';
+
+function Header(props) {
+    const { onDrawerToggle } = props;
+    const [anchorEl, setAnchorEl] = React.useState(null);
+    const open = Boolean(anchorEl);
+    const handleClick = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+
+    return (
+        <React.Fragment>
+            <AppBar color="primary" position="sticky" elevation={0}>
+                <Toolbar>
+                    <Grid container spacing={1} alignItems="center">
+                        <Grid sx={{ display: { sm: 'none', xs: 'block' } }} item>
+                            <IconButton
+                                color="inherit"
+                                aria-label="open drawer"
+                                onClick={onDrawerToggle}
+                                edge="start"
+                            >
+                                <MenuIcon />
+                            </IconButton>
+                        </Grid>
+                        <Grid item xs />
+
+                    </Grid>
+                </Toolbar>
+            </AppBar>
+            <AppBar
+                component="div"
+                color="primary"
+                position="static"
+                elevation={0}
+                sx={{ zIndex: 0 }}
+            >
+                <Toolbar>
+                    <Grid container alignItems="center" spacing={1}>
+                        <Grid item xs>
+                            <Typography color="inherit" variant="h5" component="h1">
+                                Compare
+                            </Typography>
+                        </Grid>
+                        <Grid item>
+                            <Link
+                                href="/"
+                                variant="body2"
+                                sx={{
+                                    textDecoration: 'none',
+                                    color: lightColor,
+                                    '&:hover': {
+                                        color: 'common.white',
+                                    },
+                                }}
+                                rel="noopener noreferrer"
+                                target="_blank"
+                            >
+                                Go to docs
+                            </Link>
+                        </Grid>
+
+                        <Grid item>
+                            <IconButton
+                                onClick={handleClick}
+                                size="small"
+                                sx={{ ml: 3 }}
+                                aria-controls={open ? 'account-menu' : undefined}
+                                aria-haspopup="true"
+                                aria-expanded={open ? 'true' : undefined}
+                            >
+                                <Avatar sx={{ width: 50, height: 50 }}>M</Avatar>
+                            </IconButton>
+
+                            <Menu
+                                anchorEl={anchorEl}
+                                id="account-menu"
+                                open={open}
+                                onClose={handleClose}
+                                onClick={handleClose}
+                                PaperProps={{
+                                    elevation: 0,
+                                    sx: {
+                                        overflow: 'visible',
+                                        filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
+                                        mt: 1.5,
+                                        '& .MuiAvatar-root': {
+                                            width: 32,
+                                            height: 32,
+                                            ml: -0.5,
+                                            mr: 1,
+                                        },
+                                        '&:before': {
+                                            content: '""',
+                                            display: 'block',
+                                            position: 'absolute',
+                                            top: 0,
+                                            right: 14,
+                                            width: 10,
+                                            height: 10,
+                                            bgcolor: 'background.paper',
+                                            transform: 'translateY(-50%) rotate(45deg)',
+                                            zIndex: 0,
+                                        },
+                                    },
+                                }}
+                                transformOrigin={{ horizontal: 'right', vertical: 'top' }}
+                                anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
+                            >
+                                <MenuItem>
+                                    <Avatar /> Profile
+                                </MenuItem>
+
+                                <Divider />
+
+                                <MenuItem>
+                                    <ListItemIcon>
+                                        <Logout fontSize="small" />
+                                    </ListItemIcon>
+                                    Logout
+                                </MenuItem>
+                            </Menu>
+                        </Grid>
+                    </Grid>
+                </Toolbar>
+            </AppBar>
+
+        </React.Fragment>
+    );
+}
+
+Header.propTypes = {
+    onDrawerToggle: PropTypes.func.isRequired,
+};
+
+export default Header;
--- src/index.js
+++ src/index.js
@@ -2,16 +2,30 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {BrowserRouter, Routes, Route} from "react-router-dom";
 
+import Compare from './components/headers/header3'
+import Company from './components/headers/header2'
+import Header from './components/headers/header'
+// import { ThemeProvider } from '@mui/material/styles';
+// import { dashboardTheme } from './pages/dashbordTheme';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    // <ThemeProvider theme={dashboardTheme}>
+    <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<App />}>
+                <Route path="index" element={<Header />} />
+                <Route path="companies" element={<Company />} />
+                <Route path="compare" element={<Compare />} />
+                {/*<Route path="profile" element={<Hosting />} />*/}
+                {/*<Route path="logout" element={<MachineLearning />} />*/}
+
+            </Route>
+
+        </Routes>
+    </BrowserRouter>
+    // </ThemeProvider>,
+
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
